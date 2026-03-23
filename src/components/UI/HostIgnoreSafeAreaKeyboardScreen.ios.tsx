9f5e7fa15337abf7c30fef9985dce9749d7def54
Tomoya Masaki <muton.tm.714@gmail.com>
2026-03-23 09:32:20 +0900

    fix

--- src/components/UI/HostIgnoreSafeAreaKeyboardScreen.ios.tsx
+++ src/components/UI/HostIgnoreSafeAreaKeyboardScreen.ios.tsx
@@ -1,35 +1,21 @@
-import { Host, TextField } from "@expo/ui/swift-ui";
+import { Host, Text } from "@expo/ui/swift-ui";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { View } from "react-native";
-import { KeyboardProvider, KeyboardStickyView } from "react-native-keyboard-controller";
 
 function HostIgnoreSafeAreaKeyboardScreen() {
   const bottomOffset = useBottomTabBarHeight();
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
-      <KeyboardStickyView
-        style={{
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          padding: 16,
-          backgroundColor: "green",
-        }}
-        offset={{ opened: bottomOffset }}
-      >
+
         <Host matchContents ignoreSafeArea="keyboard" style={{ backgroundColor: "red" }}>
-          <TextField placeholder="Enter text" multiline />
+          <Text>This is a test</Text>
         </Host>
-      </KeyboardStickyView>
     </View>
   );
 }
 
 export default function HostIgnoreSafeAreaKeyboardScreenWrapper() {
   return (
-    <KeyboardProvider>
       <HostIgnoreSafeAreaKeyboardScreen />
-    </KeyboardProvider>
   );
 }
